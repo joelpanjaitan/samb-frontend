a953a681357a2b722a60b1d4afce0d81571b98ab
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-11-21 20:10:18 +0700

    update for app displays

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Warehouse management system</p>
-        <a
+        <p className="text-3xl pb-3">Warehouse management system</p>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <div className="flex gap-2">
+          <button className="text-base App-link bg-transparent border border-green-200 rounded-2xl px-4">
+            Penerimaan barang
+          </button>
+          <button className="text-base App-link bg-transparent border border-green-200 rounded-2xl px-4">
+            Pengeluaran barang
+          </button>
+          <button className="text-base App-link bg-transparent border border-green-200 rounded-2xl px-4">
+            Inventory Report
+          </button>
+        </div>
       </header>
     </div>
   );
