a531b5e0da8e8c5fb559a4db8c5a2637da3318d2
Joel Panjaitan <joel.martuaraja@praktis.co>
2024-11-22 12:49:48 +0700

    update for stock report page here

--- src/pages/StockReport.tsx
+++ src/pages/StockReport.tsx
@@ -3,28 +3,30 @@ function StockReport() {
   const { stock } = StockInventoryComponent();
   return (
     <>
-      <div>
-        <h2>Stock Report</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Gudang</th>
-              <th>Produk</th>
-              <th>Qty Dus</th>
-              <th>Qty Pcs</th>
-            </tr>
-          </thead>
-          <tbody>
-            {stock.map((item, index) => (
-              <tr key={index}>
-                <td>{item.WarehouseName}</td>
-                <td>{item.ProductName}</td>
-                <td>{item.QtyDus}</td>
-                <td>{item.QtyPcs}</td>
+      <div className="bg-[#282c34] h-screen text-[#61dafb] flex justify-center">
+        <div className="w-2/4 p-3 flex flex-col">
+          <h2 className="mb-3">Stock Report</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Gudang</th>
+                <th>Produk</th>
+                <th>Qty Dus</th>
+                <th>Qty Pcs</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {stock.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.WarehouseName}</td>
+                  <td>{item.ProductName}</td>
+                  <td>{item.QtyDus}</td>
+                  <td>{item.QtyPcs}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
